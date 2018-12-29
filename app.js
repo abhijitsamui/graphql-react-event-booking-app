2ad64751da9eff0658ddfa5afc1deab7c67bfc0c
Abhijit Samui <abhijit.samui@gmail.com>
2018-12-29 11:19:37 -0700

    initial graphql Express setup

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 app.use(express.json());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+    type RootQuery {
+        events: [String!]!
+    }
+    type RootMutation {
+        createEvent(name: String): String
+    }
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+    rootValue: {
+      events: () => {
+        return ["Movie booking", "Event Booking", "Marriage Event"];
+      },
+      createEvent: args => {
+        const eventName = args.name;
+        return eventName;
+      }
+    },
+    graphiql: true
+  })
+);
 
 app.listen(3000, () => console.log("Server Listening on PORT 3000..."));
 
