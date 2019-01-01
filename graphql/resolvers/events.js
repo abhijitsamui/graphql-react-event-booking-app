828f5f19c1e67e313bc4103db6a801b4bd008944
Abhijit Samui <abhijit.samui@gmail.com>
2018-12-31 22:47:27 -0700

    code refactoring & clean coding

---
+++ graphql/resolvers/events.js
@@ -0,0 +1,43 @@
+const Event = require("../../models/event");
+const { dateToString } = require("../../helpers/date");
+const { transformEvent } = require("./merge");
+
+module.exports = {
+  events: async () => {
+    try {
+      const events = await Event.find();
+
+      return events.map(event => {
+        return transformEvent(event);
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+  createEvent: async args => {
+    let createdEvent;
+    try {
+      const event = new Event({
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: dateToString(args.eventInput.date),
+        creator: "5c2ab355a04bfe4ad08564de"
+      });
+      const result = await event.save();
+
+      createdEvent = transformEvent(result);
+      const creator = await User.findById("5c2ab355a04bfe4ad08564de");
+
+      if (!creator) {
+        throw new Error("USER NOT FOUND!!");
+      }
+      creator.createdEvents.push(event);
+      await creator.save();
+      return createdEvent;
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  }
+};
